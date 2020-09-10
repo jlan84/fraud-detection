c2fa5af0f1c22b7f7f5fabb45d8fbf133cea55bf
Martha Wood <woodmc10@gmail.com>
2020-09-10 16:29:36 -0600

    start flask app

--- mw_predict.py
+++ mw_predict.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import cPickle as pickle
+import pickle
 import psycopg2 as pg2
-from config import config    # from the Postgres webpage (not sure what it does) 
+# from config import config    # from the Postgres webpage (not sure what it does) 
 
 def insert_event(event, prediction):
     '''Insert a new event into the event table'''
@@ -31,19 +31,34 @@ def insert_event(event, prediction):
                 conn.close
     return event_id
 
+def vectorize_single(event):
+    # drop_list = ['acct_type', 'approx_payout_date', 'event_end', 'event_start',
+    #              'gts', 'num_payouts', 'payout_type', 'sale_duration',
+    #              'sale_duration2','ticket_types']
+    keep_list = ['body_length', 'channels', 'delivery_method', 'event_created',
+       'event_published', 'fb_published', 'has_analytics', 'has_header',
+       'has_logo', 'name_length', 'num_order', 'object_id', 'org_facebook',
+       'org_twitter', 'show_map', 'user_age', 'user_created', 'user_type',
+       'venue_latitude', 'venue_longitude']
+    event_dropped = event[keep_list]
+    event_filled = event_dropped.fillna(0)
+    event_for_model = event_filled._get_numeric_data()
+    return event_for_model
+
 if __name__ == '__main__':
     # Read in single example
-    event = pd.read_csv('data/test_script_examples.csv')
+    event = pd.read_csv('data/test_script_examples.csv', nrows=1, index_col='Unnamed: 0')
 
     # Vectorize example
     vc_event = vectorize_single(event)
+    print(vc_event.columns)
 
     # Unpickle the model
-    with open ('model.pkl') as f_un:
+    with open ('data/model.pkl', 'rb') as f_un:
         model_unpickled = pickle.load(f_un)
 
     # Predict the label
-    prediction = model_unpickled.predict_proba(event)
+    prediction = model_unpickled.predict_proba(vc_event)
 
     # Output label probability
     print(prediction)
@@ -51,4 +66,4 @@ if __name__ == '__main__':
     # Add to Postgres/Mongo DB
     # ---Still need to create database, starting with simplest option I can
     # ---think of: one column for the event, one column for the prediction
-    insert_event(event, prediction)
+    # insert_event(event, prediction)
