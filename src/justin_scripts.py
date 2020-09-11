d40e88cb6b98478bf88f7703d1f5002b332d3130
Justin Lansdale <justinlansdale@Justins-MacBook-Pro.local>
2020-09-11 11:34:32 -0600

    updated justin files

--- src/justin_scripts.py
+++ src/justin_scripts.py
@@ -21,6 +21,7 @@ import re
 from collections import Counter
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+import pickle as pickle
 
 
 plt.style.use('fivethirtyeight')
@@ -39,6 +40,20 @@ def text_from_html(body):
     visible_texts = filter(tag_visible, texts)  
     return u" ".join(t.strip() for t in visible_texts)
 
+def process_one(df, stop_words):
+        df['desc_text'] = text_from_html(df.loc['description'])
+        lower = df['desc_text'].lower()
+        split = lower.split()
+        stops = [w for w in split if w not in stop_words]
+        stop_num = [w for w in stops if not (w.isdigit() 
+                        or w[0] == '-' and w[1:].isdigit())]
+        X = ' '.join(stop_num)
+        X = [X]
+        # cv = CountVectorizer()
+        # count_mx = cv.fit_transform(X)
+        # tfidf_mx = tfidf_matrix.transform(count_mx)
+        return X
+
 class NaiveBayes():
 
     def __init__(self, df, stop_words):
@@ -49,10 +64,20 @@ class NaiveBayes():
         self.nb_model = None
         self.stop_words = stop_words
 
+    def description_to_text(self):
+        self.df['desc_text'] = 0
+        for i in range(self.df.shape[0]):
+            self.df.loc[i,'desc_text'] = text_from_html(self.df.loc[i,'description'])
+        
+    def sub_set_df(self):
+        df_fraud = self.df[self.df['fraud'] == 1]
+        df_nf = self.df[self.df['fraud'] == 0]
+        df_nf_subset = df_nf.sample(n=df_fraud.shape[0], replace=False, random_state=1720)
+        self.df_combined = df_fraud.append(df_nf_subset, ignore_index=True)
 
     def generate_train_test(self, train_size=0.75):
-        self.y = self.df.pop('fraud').values
-        self.X = self.df['desc_text'].values
+        self.y = self.df_combined.pop('fraud').values
+        self.X = self.df_combined['desc_text'].values
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
                                                                 self.X, self.y, 
                                                                 stratify=self.y, 
@@ -90,7 +115,7 @@ class NaiveBayes():
                         or w[0] == '-' and w[1:].isdigit())]
             self.stops_removed_lst.append(stop_num)
         self.X_test = self.join_list_of_strings(self.stops_removed_lst)
-
+    
     def tf_idf_matrix(self):
             """
             Sets up a word count matrix, a tfidf matrix, and a CountVectorizer for
@@ -106,8 +131,8 @@ class NaiveBayes():
             """
             self.train_cv = CountVectorizer()
             self.train_count_matrix = self.train_cv.fit_transform(self.X_train)
-            tfidf_transformer = TfidfTransformer()
-            self.train_tfidf_matrix = tfidf_transformer.fit_transform(self.train_count_matrix)
+            self.tfidf_transformer = TfidfTransformer()
+            self.train_tfidf_matrix = self.tfidf_transformer.fit_transform(self.train_count_matrix)
 
     def naive_bayes_model(self):
             """
@@ -124,6 +149,20 @@ class NaiveBayes():
             """
             self.nb_model = MultinomialNB()
             self.nb_model.fit(self.train_tfidf_matrix, self.y_train)
+
+    def pickle_model(self):
+            self.description_to_text()
+            self.sub_set_df()
+            self.generate_train_test()
+            self.remove_X_train_stops()
+            self.remove_X_test_stops()
+            self.tf_idf_matrix()
+            nb = MultinomialNB()
+            nb.fit(self.train_tfidf_matrix, self.y_train)
+            with open('bayes.pkl', 'wb') as f:
+                pickle.dump(nb,f)
+            with open('tfidf.pkl', 'wb') as f:
+                pickle.dump(self.tfidf_transformer ,f)
 
     def return_top_n_words(self, n=7):
             """
@@ -167,6 +206,8 @@ class NaiveBayes():
                             ])
 
         self.nb_pipeline.fit(self.X_train, self.y_train)
+        with open('nb_pipeline.pkl', 'wb') as f:
+            pickle.dump(self.nb_pipeline, f)
         self.predicted = self.nb_pipeline.predict(self.X_test)
         self.accuracy = np.mean(self.predicted == self.y_test)
         print("\nThe accuracy on the test set is {0:0.3f}.".format(self.accuracy))
@@ -189,65 +230,37 @@ class NaiveBayes():
         return self.nb_pipeline.predict_proba(X)
 
 if __name__ == "__main__":
-    acct_type_ct = df.acct_type.value_counts(dropna=False, ascending=True)
-    # acct_type_ct.plot.barh(x=acct_type_ct.index, y=acct_type_ct.iloc[0])
-    # plt.title('Account Types')
-    # plt.tight_layout()
-    # plt.show()
 
-    # ticket_types = df.ticket_types
-    # print(ticket_types.iloc[0])
-    # previous_payouts = df.previous_payouts
-    # print(previous_payouts.iloc[1])
 
-    acct_type_grp = df.groupby('acct_type').count().reset_index()
-
-    # print(acct_type_grp[['acct_type','org_twitter']])
-    org_desc = df.org_desc.value_counts(dropna=False)
-    # print(org_desc)
-
-    descriptions = df.description
     df['fraud'] = np.where((df['acct_type'] == 'fraudster') | 
                         (df['acct_type'] == 'fraudster_event') |
                         (df['acct_type'] == 'fraudster_att'), 1, 0)
-    descriptions = df.description
-    df['desc_text'] = 0
-    print(df.info())
-    for i in range(df.shape[0]):
-        df.iloc[i,45] = text_from_html(descriptions[i])
-    
-    df_fraud = df[df['fraud'] == 1]
-    df_nf = df[df['fraud'] == 0]
-    df_nf_subset = df_nf.sample(n=df_fraud.shape[0], replace=False)
-    df_combined = df_fraud.append(df_nf_subset, ignore_index=True)
-    
-
-
-
     stop_words = stopwords.words('english')
-    extra_stops = ['tickets', '00']
+    extra_stops = ['tickets', 00, 'event']
     stop_words.extend(extra_stops)
 
-    nb = NaiveBayes(df_combined, stop_words)
-    nb.generate_train_test()
-    nb.remove_X_train_stops()
-    nb.remove_X_test_stops()
+    with open('nb_pipeline.pkl', 'rb') as f:
+        nb_pipeline = pickle.load(f)
     
-    test_counts_fraud = [x for x in nb.y_test if x == 1]
-    test_counts_notfraud = [x for x in nb.y_test if x == 0]
-    train_counts_fraud = [x for x in nb.y_train if x == 1]
-    train_counts_notfraud = [x for x in nb.y_train if x == 0]
-    print(len(test_counts_fraud)/len(test_counts_notfraud))
-    print(len(train_counts_fraud)/len(train_counts_notfraud))
-
-    nb.tf_idf_matrix()
-    nb.naive_bayes_model()
-    nb.return_top_n_words()
-    nb.get_accuracy_classification_report()
-    probs = nb.predic_probablility(df['description'])
-    print(type(probs))
-    print(probs.shape)
-    print(p)
+    new_df = df.iloc[1,:]
+    X = process_one(new_df, stop_words)
+    print(nb_pipeline.predict_proba(X))
+    # nb = NaiveBayes(df, stop_words)
+    # nb.description_to_text()
+    # nb.sub_set_df()
+    # nb.generate_train_test()
+    # nb.remove_X_train_stops()
+    # nb.remove_X_test_stops()
+    
+    # nb.tf_idf_matrix()
+    # nb.naive_bayes_model()
+    # nb.return_top_n_words()
+    # nb.get_accuracy_classification_report()
+    # print(nb.class_report)
+    # # probs = nb.predic_probablility(df['description'])
+    # # print(type(probs))
+    # # print(probs.shape)
+    # # print(p)
 
     # fig, ax = plt.subplots(figsize=(12,12))
     # nb.confustion_matrix_plot(ax)
